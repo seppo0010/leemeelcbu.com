8c827b52fab4080247cae32aed27fe6c3a71d851
Sebastian Waisbrot <seppo0010@gmail.com>
2022-08-01 19:38:18 -0300

    Support PDF

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,39 @@ import './App.css';
 import Tesseract from 'tesseract.js';
 import Dropzone from 'react-dropzone'
 import ReactLoading from 'react-loading';
+import * as pdfjsLib from "pdfjs-dist";
+
+pdfjsLib.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/2.15.349/pdf.worker.js`;
+
+function findCBUsInText(text: string) {
+  return Array.from(text.matchAll(/[0-9\- ]+/g))
+    .map((res) => res[0].replace(/\D/g,''))
+    .filter((res) => res.length === 22)
+}
+async function readImage(f: File) {
+  if (f.type.split('/')[0] !== 'image') return [];
+  return await Tesseract.recognize(
+    f,
+    'spa',
+    { logger: m => console.log(m) }
+  ).then(({ data: { text } }) => findCBUsInText(text))
+}
+
+async function readPDF(f: File) {
+  if (f.type !== 'application/pdf') return [];
+  const loadingTask = pdfjsLib.getDocument(new Uint8Array(await f.arrayBuffer()));
+  const pdf = await loadingTask.promise;
+  let cbus: string[] = [];
+
+  for (let i = 0; i < pdf.numPages; i++) {
+    const page = await pdf.getPage(i+1);
+    const texts = await page.getTextContent();
+    for (const item of texts.items) {
+      cbus = [...cbus, ...findCBUsInText((item as any).str || '')]
+    }
+  }
+  return cbus;
+}
 
 function App() {
   const [results, setResults] = useState<string[] | null>(null);
@@ -11,18 +44,13 @@ function App() {
   const onDrop = (acceptedFiles: File[]) => {
     setResults(null);
     setProgress(0);
-    acceptedFiles.forEach((f) => {
-      Tesseract.recognize(
-        f,
-        'spa',
-        { logger: m => console.log(m) }
-      ).then(({ data: { text } }) => {
-        const newResults = Array.from(text.matchAll(/[0-9\- ]+/g))
-          .map((res) => res[0].replace(/\D/g,''))
-          .filter((res) => res.length === 22)
-        setResults(newResults);
-        setProgress(1);
-      })
+    acceptedFiles.forEach(async (f) => {
+      const [resultsPDF, resultsImage] = await Promise.allSettled([readPDF(f), readImage(f)]);
+      setResults([
+        ...(resultsPDF.status === 'fulfilled' ? resultsPDF.value : []),
+        ...(resultsImage.status === 'fulfilled' ? resultsImage.value : []),
+      ]);
+      setProgress(1);
     })
   }
   return (
@@ -39,7 +67,7 @@ function App() {
       {results !== null && <div id="results">
         {results.length === 0 && <p>No se encontraron CBUs</p>}
         {results.length > 0 && <div>
-          <ul>{results.map((r: string) => <li key={r}>{r}</li>)}</ul>
+          <ul>{results.map((r: string, i) => <li key={i}>{r}</li>)}</ul>
           <button onClick={() => { setResults(null); setProgress(null); }}>Volver a empezar</button>
         </div>}
       </div>}
