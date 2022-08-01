7eb3d6350835e5b0675d08679ce00401d24b39fd
Sebastian Waisbrot <seppo0010@gmail.com>
2022-08-01 15:20:03 -0300

    First OCR version

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import Tesseract from 'tesseract.js';
+import Dropzone from 'react-dropzone'
 
 function App() {
+  const [results, setResults] = useState<string[] | null>(null);
+  const [progress, setProgress] = useState<number | null>(null);
+
+  const onDrop = (acceptedFiles: File[]) => {
+    setResults(null);
+    setProgress(0);
+    acceptedFiles.forEach((f) => {
+      Tesseract.recognize(
+        f,
+        'spa',
+        { logger: m => console.log(m) }
+      ).then(({ data: { text } }) => {
+        const newResults = Array.from(text.matchAll(/[0-9\- ]+/g))
+          .map((res) => res[0].replace(/\D/g,''))
+          .filter((res) => res.length === 22)
+        setResults(newResults);
+        setProgress(1);
+      })
+    })
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Dropzone onDrop={onDrop}>
+        {({getRootProps, getInputProps}) => (
+          <div {...getRootProps()}>
+            <input {...getInputProps()} />
+            <p>Subime tu imagen con un CBU acá</p>
+          </div>
+        )}
+      </Dropzone>
+      {results === null && progress !== null && <p>Procesando</p>}
+      {results !== null && results.length === 0 && <p>No se encontraron CBUs</p>}
+      {results && results.length && <ul>
+        {results.map((r: string) => <li key={r}>{r}</li>)}
+      </ul>}
     </div>
   );
 }
